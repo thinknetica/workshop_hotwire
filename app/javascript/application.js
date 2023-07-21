a259869dc841275625e63721b8462a7f2e292e27
Vladimir Dementyev <palkan@hey.com>
2023-07-20 20:37:56 -0700

    event.details.render = morphdom

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
+import morphdom from "morphdom";
+
+let prevPath = window.location.pathname;
+
+document.addEventListener("turbo:before-render", (event) => {
+  Turbo.navigator.currentVisit.scrolled = prevPath === window.location.pathname;
+  prevPath = window.location.pathname;
+  event.detail.render = async (prevEl, newEl) => {
+    await new Promise((resolve) => setTimeout(() => resolve(), 0));
+    morphdom(prevEl, newEl);
+  };
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,4 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.3.0/app/javascript/turbo/index.js"
 pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.3.0/dist/turbo.es2017-esm.js"
 pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.6/src/index.js"
+pin "morphdom", to: "https://ga.jspm.io/npm:morphdom@2.7.0/dist/morphdom-esm.js"

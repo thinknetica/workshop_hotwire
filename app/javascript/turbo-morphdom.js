f47661c0ddf869e7b014832d0e928f1ae03771c6
Vladimir Dementyev <palkan@hey.com>
2023-07-23 14:37:41 -0700

    turbo-morphdom.js

---
+++ app/javascript/turbo-morphdom.js
@@ -0,0 +1,52 @@
+import morphdom from "morphdom";
+
+let prevPath = window.location.pathname;
+
+document.addEventListener("turbo:before-render", (event) => {
+  Turbo.navigator.currentVisit.scrolled = prevPath === window.location.pathname;
+  prevPath = window.location.pathname;
+  event.detail.render = async (prevEl, newEl) => {
+    await new Promise((resolve) => setTimeout(() => resolve(), 0));
+    morphdom(prevEl, newEl);
+  };
+
+  if (document.startViewTransition) {
+    event.preventDefault();
+
+    document.startViewTransition(() => {
+      event.detail.resume();
+    });
+  }
+});
+
+document.addEventListener("turbo:before-frame-render", (event) => {
+  event.detail.render = (currentElement, newElement) => {
+    if (
+      currentElement.hasAttribute("transition-name") &&
+      newElement.hasAttribute("transition-name") &&
+      currentElement.firstElementChild?.getAttribute("transition-id") !==
+        newElement.firstElementChild?.getAttribute("transition-id")
+    ) {
+      document.documentElement.setAttribute(
+        "transition",
+        currentElement.getAttribute("transition-name")
+      );
+    }
+
+    morphdom(currentElement, newElement, {
+      childrenOnly: true,
+    });
+  };
+
+  if (document.startViewTransition) {
+    event.preventDefault();
+
+    document
+      .startViewTransition(() => {
+        event.detail.resume();
+      })
+      .finished.then(() => {
+        document.documentElement.removeAttribute("transition");
+      });
+  }
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7
 pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.3.0/dist/turbo.es2017-esm.js"
 pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.6/src/index.js"
 pin "morphdom", to: "https://ga.jspm.io/npm:morphdom@2.7.0/dist/morphdom-esm.js"
+pin "turbo-morphdom", to: "turbo-morphdom.js"

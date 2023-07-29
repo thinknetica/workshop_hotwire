a7e0f7283e6d1862c0ca5c532e3766f8c304dcea
Evgeny Varnakov <evgeniy.varnakov@sbermarket.ru>
2023-07-29 15:41:28 +0300

    solution

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,10 +2,14 @@ class ArtistsController < ApplicationController
   def show
     artist = Artist.find(params[:id])
     albums = selected_albums(artist.albums, params[:album_type]).with_attached_cover.preload(:artist)
-    tracks = artist.tracks.popularity_ordered.limit(5)
+    tracks = artist.tracks.popularity_ordered.limit(5 * (params[:tracks_page].to_i + 1))
 
     if turbo_frame_request?
-      render partial: "discography", locals: {artist:, albums:}
+      if params[:tracks_page].present?
+        render partial: "tracks", locals: {artist:, tracks:}
+      else
+        render partial: "discography", locals: {artist:, albums:}
+      end
     else
       render action: :show, locals: {artist:, albums:, tracks:}
     end

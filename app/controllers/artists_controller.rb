28c55574d21f35adcc5b20e0d810fa3850378644
a.mukharryamov <a.mukharryamov@fun-box.ru>
2023-07-30 19:55:32 +0300

    Load more popular tracks

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,10 +2,14 @@ class ArtistsController < ApplicationController
   def show
     artist = Artist.find(params[:id])
     albums = selected_albums(artist.albums, params[:album_type]).with_attached_cover.preload(:artist)
-    tracks = artist.tracks.popularity_ordered.limit(5)
+    tracks = artist.tracks.popularity_ordered.limit(select_limit(params[:tracks_number]))
 
     if turbo_frame_request?
-      render partial: "discography", locals: {artist:, albums:}
+      if params[:tracks_number].present?
+        render partial: "tracks", locals: {artist:, tracks:}
+      else
+        render partial: "discography", locals: {artist:, albums:}
+      end
     else
       render action: :show, locals: {artist:, albums:, tracks:}
     end
@@ -19,5 +23,11 @@ class ArtistsController < ApplicationController
     return albums.lp unless Album.kinds.key?(album_type)
 
     albums.where(kind: album_type)
+  end
+
+  def select_limit(tracks_number)
+    tracks_count = tracks_number.to_i + 5
+
+    tracks_count > 20 ? 20 : tracks_count
   end
 end

0373568493c5913411986e51155fea8e00f487fc
a.mukharryamov <a.mukharryamov@fun-box.ru>
2023-08-01 15:19:33 +0300

    Show all tracks button

--- Gemfile
+++ Gemfile
@@ -42,6 +42,9 @@ gem "freezolite", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug"
+
+  gem 'pry-rails'
+  gem 'pry-byebug'
 end
 
 group :development do
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,6 +11,18 @@ class ArtistsController < ApplicationController
     end
   end
 
+  def tracks
+    artist = Artist.find(params[:id])
+    albums = selected_albums(artist.albums, params[:album_type]).with_attached_cover.preload(:artist)
+    tracks = artist.tracks.popularity_ordered
+
+    if turbo_frame_request?
+      render partial: "tracks", locals: {artist:, tracks:}
+    else
+      render action: :show, locals: {artist:, albums:, tracks:}
+    end
+  end
+
   private
 
   def selected_albums(albums, album_type)

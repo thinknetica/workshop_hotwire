d6db6c73efba2e9de794ed69688d4ef9a5bcab43
Igor Ladkin <dgkplan@googlemail.com>
2023-08-13 23:45:30 +0200

    Infinite scroll on artist tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,7 +1,32 @@
 class TracksController < ApplicationController
-  def index
-    artist = Artist.find(params[:artist_id])
+  helper_method :current_page, :has_next_page?, :next_page
 
-    render action: :index, locals: {artist:}
+  def index
+    @artist = Artist.find(params[:artist_id])
+    tracks = @artist.tracks.offset(current_page * per_page).limit(per_page)
+
+    if turbo_frame_request? && turbo_frame_request_id.include?("tracks")
+      render partial: "tracks", locals: {artist: @artist, tracks:}
+    else
+      render action: :index, locals: {artist: @artist, tracks:}
+    end
+  end
+
+  private
+
+  def per_page
+    10
+  end
+
+  def current_page
+    (params[:page] || 1).to_i
+  end
+
+  def has_next_page?
+    current_page * 10 < @artist.tracks.size
+  end
+
+  def next_page
+    current_page + 1
   end
 end

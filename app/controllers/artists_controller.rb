94c672eaa423af28a7416d5e5511218722323de7
Igor Ladkin <dgkplan@googlemail.com>
2023-08-13 22:14:18 +0200

    Load more tracks when clicking on the link

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,7 +2,7 @@ class ArtistsController < ApplicationController
   def show
     artist = Artist.find(params[:id])
     albums = selected_albums(artist.albums, params[:album_type]).with_attached_cover.preload(:artist)
-    tracks = artist.tracks.popularity_ordered.limit(5)
+    tracks = artist.tracks.popularity_ordered.limit(popular_tracks_limit)
 
     if turbo_frame_request? && turbo_frame_request_id.include?("discography")
       render partial: "discography", locals: {artist:, albums:}
@@ -21,5 +21,9 @@ class ArtistsController < ApplicationController
     return albums.lp unless Album.kinds.key?(album_type)
 
     albums.where(kind: album_type)
+  end
+
+  def popular_tracks_limit
+    params[:popular_tracks_limit] || 5
   end
 end

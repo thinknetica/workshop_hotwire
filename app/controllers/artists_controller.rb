b2f0bbd69ac23d80b8bf1561f5762d3e8a765594
Igor Ladkin <dgkplan@googlemail.com>
2023-08-13 22:35:57 +0200

    Add helper to check if the artist has more popular tracks to load

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,15 +1,17 @@
 class ArtistsController < ApplicationController
+  helper_method :has_more_popular_tracks?
+
   def show
-    artist = Artist.find(params[:id])
-    albums = selected_albums(artist.albums, params[:album_type]).with_attached_cover.preload(:artist)
-    tracks = artist.popular_tracks.limit(popular_tracks_limit)
+    @artist = Artist.find(params[:id])
+    albums = selected_albums(@artist.albums, params[:album_type]).with_attached_cover.preload(:artist)
+    tracks = @artist.popular_tracks.limit(popular_tracks_limit)
 
     if turbo_frame_request? && turbo_frame_request_id.include?("discography")
-      render partial: "discography", locals: {artist:, albums:}
+      render partial: "discography", locals: {artist: @artist, albums:}
     elsif turbo_frame_request? && turbo_frame_request_id.include?("popular-tracks")
-      render action: :show, locals: {artist:, albums:, tracks:}
+      render action: :show, locals: {artist: @artist, albums:, tracks:}
     else
-      render action: :show, locals: {artist:, albums:, tracks:}
+      render action: :show, locals: {artist: @artist, albums:, tracks:}
     end
   end
 
@@ -24,6 +26,10 @@ class ArtistsController < ApplicationController
   end
 
   def popular_tracks_limit
-    params[:popular_tracks_limit] || 5
+    (params[:popular_tracks_limit].presence || 5).to_i
+  end
+
+  def has_more_popular_tracks?
+    @artist.popular_tracks.count > popular_tracks_limit
   end
 end
